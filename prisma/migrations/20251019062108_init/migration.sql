4dcc2ddb714ed2f00ea875ba316db67e2d475214
AngusWArthur <21918446@students.ltu.edu.au>
2025-10-21 09:46:49 +1000

    Attempt to add cloud functionality using NeonDB

--- prisma/migrations/20251019062108_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "TabSet" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "tabs" JSONB NOT NULL,
-    "html" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
